5755f400cfcaf5506a2186187c072e64ef8314b0
RAGHAV MANDLOI <raghavmandloi2004@gmail.com>
2026-06-19 11:48:32 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,10 @@
+import os
 from supabase import create_client
+from dotenv import load_dotenv
 
-SUPABASE_URL = "https://ffbjfsjljfxxzhilolek.supabase.co"
-SUPABASE_KEY = "sb_publishable_l_9-b2r27I4ajIBfoaHqSA_UN5ALUdo"
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_SECRET_KEY")  # use secret key for backend
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
