26772d3d91aff64fcad6b9b3ae8acf6fae65d48c
RAGHAV MANDLOI <raghavmandloi2004@gmail.com>
2026-06-21 13:56:31 +0530

    Fix CORS origin URL for yokaiecom

--- app/app.py
+++ app/app.py
@@ -23,7 +23,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
          "https://ecom-project-07.vercel.app",
-         "yokaiecom.vercel.app"
+         "https://yokaiecom.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
