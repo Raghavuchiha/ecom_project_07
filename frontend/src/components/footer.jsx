2995d2ddb238a9e9a3bc546d73dc832398868ec2
RAGHAV MANDLOI <raghavmandloi2004@gmail.com>
2026-06-30 23:00:37 +0530

    Clean up footer component structure
    Removed unused div and comments from the footer component.

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -3,14 +3,6 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
-      
-      {/* Logo and Description */}
-      <div>
-      
-        <p className="w-full md:w-2/3 text-gray-600">
-      
-      </div>
 
       {/* Company Links */}
       <div>
